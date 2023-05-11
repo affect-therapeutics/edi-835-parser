762f8b2078c723812b92dac0e913be697fd51671
Russell Osborne <russell@joinaffect.com>
2023-05-10 23:40:56 -0400

    Addresses Review to continue to output service data
    Signed-off-by: Russell Osborne <russell@joinaffect.com>

--- edi_835_parser/transaction_set/transaction_set.py
+++ edi_835_parser/transaction_set/transaction_set.py
@@ -37,9 +37,23 @@ class TransactionSet:
 						claim,
 						service,
 						transaction
+
 					)
 
-				data.append(datum)
+					for index, adjustment in enumerate(service.adjustments):
+							datum[f'adj_{index}_group'] = adjustment.group_code.code
+							datum[f'adj_{index}_code'] = adjustment.reason_code.code
+							datum[f'adj_{index}_amount'] = adjustment.amount
+
+					for index, reference in enumerate(service.references):
+							datum[f'ref_{index}_qual'] = reference.qualifier.code
+							datum[f'ref_{index}_value'] = reference.value
+
+					for index, remark in enumerate(service.remarks):
+							datum[f'rem_{index}_qual'] = remark.qualifier.code
+							datum[f'rem_{index}_code'] = remark.code.code
+
+					data.append(datum)
 
 		data = pd.DataFrame(data)
 
@@ -67,7 +81,7 @@ class TransactionSet:
 
 		datum = {
 			'edi_transacrion_id_st02': transaction.transaction.transaction_set_control_no,
-			'patient_control_id': claim.claim.patient_control_number,
+			'patient_control_id': claim.claim.patient_control_number if claim.claim else None,
 			'patient_id_qualifier': claim.patient.identification_code_qualifier,
 			'patient_id': claim.patient.identification_code,
 			'patient_last_name': claim.patient.last_name,
@@ -75,39 +89,38 @@ class TransactionSet:
 			'patient_middle_name': claim.patient.middle_name,
 			'patient_name_suffix': claim.patient.name_suffix,
 			'patient_name_prefix': claim.patient.name_prefix,
-			'code': service.service.code,
-			'modifier': service.service.modifier,
-			'qualifier': service.service.qualifier,
-			'allowed_units': service.service.allowed_units,
-			'billed_units': service.service.billed_units,
-			'charge_amount': service.service.charge_amount,
-			'allowed_amount': service.allowed_amount,
-			'paid_amount': service.service.paid_amount,
-			'start_date': start_date,
-			'end_date': end_date,
+			'service_code': service.service.code,
+			'service_modifier': service.service.modifier,
+			'service_qualifier': service.service.qualifier,
+			'service_allowed_units': service.service.allowed_units,
+			'service_billed_units': service.service.billed_units,
+			'service_charge_amount': service.service.charge_amount,
+			'service_paid_amount': service.service.paid_amount,
+			'service_start_date': start_date,
+			'service_end_date': end_date,
 			'payer_id': [o.organization.type for o in transaction.organizations if o.organization.type == 'payer'][0],
 			'payer_name': [o.organization.name for o in transaction.organizations if o.organization.type == 'payer'][0],
-			'bt_facility_type_code_clp08': claim.claim.facility_type_code,
-			'bt_facility_type_code_clp09': claim.claim.claim_frequency_code,
+			'bt_facility_type_code_clp08': claim.claim.facility_type_code if claim.claim else None,
+			'bt_facility_type_code_clp09': claim.claim.claim_frequency_code if claim.claim else None,
 			'payee_id_qualifier': [o.organization.identification_code_qualifier for o in transaction.organizations if o.organization.type == 'payee'][0],
 			'payee_id': [o.organization.identification_code for o in transaction.organizations if o.organization.type == 'payee'][0],
-			'provider_entity_type_qualifier': claim.rendering_provider.type,
-			'provider_id_qualifier': claim.rendering_provider.identification_code_qualifier,
-			'provider_id': claim.rendering_provider.identification_code,
+			'provider_entity_type_qualifier': claim.rendering_provider.type if claim.rendering_provider else None,
+			'provider_id_qualifier': claim.rendering_provider.identification_code_qualifier if claim.rendering_provider else None,
+			'provider_id': claim.rendering_provider.identification_code if claim.rendering_provider else None,
 			'provider_name': claim.rendering_provider.name if claim.rendering_provider else None,
-			'provider_first_name': claim.rendering_provider.first_name,
-			'provider_middle_name': claim.rendering_provider.middle_name,
-			'provider_suffix': claim.rendering_provider.name_suffix,
-			'provider_prefix': claim.rendering_provider.name_prefix,
-			'claim_received_date': claim.claim_received_date.date,
+			'provider_first_name': claim.rendering_provider.first_name if claim.rendering_provider else None,
+			'provider_middle_name': claim.rendering_provider.middle_name if claim.rendering_provider else None,
+			'provider_suffix': claim.rendering_provider.name_suffix if claim.rendering_provider else None,
+			'provider_prefix': claim.rendering_provider.name_prefix if claim.rendering_provider else None,
+			'claim_received_date': claim.claim_received_date.date if claim.claim_recieved_date else None,
 			'claim_paid_date': transaction.financial_information.transaction_date,
-			'claim_status': claim.claim.status,
-			'claim_total_charge_amount': claim.claim.charge_amount,
-			'claim_payment_amount': claim.claim.paid_amount,
-			'claim_patient_responsibility': claim.claim.patient_responsibility_amount,
-			'claim_filing_indicator': claim.claim.claim_filing_indicator_code,
-			'payer_claim_control_number': claim.claim.payer_claim_control_number,
-			'drg_code': claim.claim.drg_code,
+			'claim_status': claim.claim.status if claim.claim else None,
+			'claim_total_charge_amount': claim.claim.charge_amount if claim.claim else None,
+			'claim_payment_amount': claim.claim.paid_amount if claim.claim else None,
+			'claim_patient_responsibility': claim.claim.patient_responsibility_amount if claim.claim else None,
+			'claim_filing_indicator': claim.claim.claim_filing_indicator_code if claim.claim else None,
+			'payer_claim_control_number': claim.claim.payer_claim_control_number if claim.claim else None,
+			'drg_code': claim.claim.drg_code if claim.claim else None,
 			'corrected_insured_last_name': claim.insured.last_name if claim.insured else None,
 			'corrected_insured_first_name': claim.insured.first_name if claim.insured else None,
 			'corrected_insured_middle_name': claim.insured.middle_name if claim.insured else None,
@@ -118,7 +131,7 @@ class TransactionSet:
 			'claim_statement_period_start': claim.claim_statement_period_start.date if claim.claim_statement_period_start else None,
 			'claim_statement_period_end': claim.claim_statement_period_end.date if claim.claim_statement_period_end else None,
 			'claim_coverage_expiration': claim.claim_coverage_expiration.date if claim.claim_coverage_expiration else None,
-			'claim_coverage_amount': claim.amount.amount,
+			'claim_coverage_amount': claim.amount.amount if claim.amount else None,
 			'claim_contract_code': claim.claim_contract_code.value if claim.claim_contract_code else None,
 		}
 
